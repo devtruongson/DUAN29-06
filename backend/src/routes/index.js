28d4e0d64ace15f830c5ef94ccd07f05a43b0ef7
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2024-07-02 14:32:11 +0700

    pull fe

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,27 +1,27 @@
 import express from "express";
 
-const adminRouter = require('./admin');
+const adminRouter = require("./admin");
 
-const customerRouter = require('./customer');
+const customerRouter = require("./customer");
 
-const categoryRouter = require('./category');
+const categoryRouter = require("./category");
 
-const productRouter = require('./product');
+const productRouter = require("./product");
 
-const productVariantRouter = require('./productVariant')
+const productVariantRouter = require("./productVariant");
 
-const orderRouter = require('./order');
+const orderRouter = require("./order");
 
-const reviewRouter = require('./review');
+const reviewRouter = require("./review");
 
 let setRoute = (app) => {
-    app.use('/api/admin', adminRouter);
-    app.use('/api/customer', customerRouter)
-    app.use('/api/category', categoryRouter);
-    app.use('/api/product', productRouter)
-    app.use('/api/productVariant', productVariantRouter)
-    app.use('/api/order', orderRouter)
-    app.use('/api/review', reviewRouter)
-}
+    app.use("/api/admin", adminRouter);
+    app.use("/api/customer", customerRouter);
+    app.use("/api/category", categoryRouter);
+    app.use("/api/product", productRouter);
+    app.use("/api/productVariant", productVariantRouter);
+    app.use("/api/order", orderRouter);
+    app.use("/api/review", reviewRouter);
+};
 
 module.exports = setRoute;
--- nodejs-clothes-web-shop/fe-portal/components/ProductManagementPage/ProductAdmin.jsx
+++ nodejs-clothes-web-shop/fe-portal/components/ProductManagementPage/ProductAdmin.jsx
@@ -1,16 +1,15 @@
-import React, { useState, useRef } from 'react'
-import Link from 'next/link'
-import axios from 'axios'
+import React, { useState, useRef } from "react";
+import Link from "next/link";
+import axios from "axios";
 import { swalert, swtoast } from "@/mixins/swal.mixin";
-import { FaTrash, FaPencilAlt } from "react-icons/fa"
-import { Switch } from 'antd';
+import { FaTrash, FaPencilAlt } from "react-icons/fa";
+import { Switch } from "antd";
 import Swal from "sweetalert2";
 
 const ProductAdmin = (props) => {
-
     const addPointToPrice = (price) => {
-        return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-    }
+        return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    };
 
     const convertTime = (created_at) => {
         const date = new Date(created_at);
@@ -26,71 +25,81 @@ const ProductAdmin = (props) => {
             <>
                 {formattedDate} <br /> {formattedTime}
             </>
-        )
-    }
+        );
+    };
 
     const handleUpdateQuantity = async () => {
         const { value: newQuantity } = await Swal.fire({
-            title: 'Nhập tồn kho mới',
-            input: 'number',
-            inputLabel: '',
-            inputPlaceholder: 'Tồn kho mới..',
+            title: "Nhập tồn kho mới",
+            input: "number",
+            inputLabel: "",
+            inputPlaceholder: "Tồn kho mới..",
             showCloseButton: true,
-        })
+        });
         if (!newQuantity) {
             swtoast.fire({
-                text: "Tồn kho sản phẩm chưa được cập nhật!"
-            })
-            return
+                text: "Tồn kho sản phẩm chưa được cập nhật!",
+            });
+            return;
         }
         if (newQuantity) {
             try {
-                await axios.put('http://localhost:8080/api/product-variant/update-quantity',
+                await axios.put(
+                    "http://localhost:8080/api/product-variant/update-quantity",
                     {
                         product_variant_ids: [props.product_variant_id],
-                        quantity: newQuantity
-                    })
-                props.refreshProductVariantTable()
+                        quantity: newQuantity,
+                    }
+                );
+                props.refreshProductVariantTable();
                 swtoast.success({
-                    text: 'Cập nhật tồn kho mới thành công!'
-                })
+                    text: "Cập nhật tồn kho mới thành công!",
+                });
             } catch (e) {
-                console.log(e)
+                console.log(e);
                 swtoast.error({
-                    text: 'Xảy ra lỗi khi cập nhật tồn kho vui lòng thử lại!'
-                })
+                    text: "Xảy ra lỗi khi cập nhật tồn kho vui lòng thử lại!",
+                });
             }
         }
-    }
+    };
 
     const [disabledInputState, setDisabledInputState] = useState(false);
 
     const handleUpdateState = async (state) => {
         if (state) {
             try {
-                setDisabledInputState(true)
-                await axios.put('http://localhost:8080/api/product-variant/on',
-                    { product_variant_ids: [props.product_variant_id] })
-                setDisabledInputState(false)
-                props.refreshProductVariantTable()
+                setDisabledInputState(true);
+                await axios.put(
+                    "http://localhost:8080/api/product-variant/on",
+                    { product_variant_ids: [props.product_variant_id] }
+                );
+                setDisabledInputState(false);
+                props.refreshProductVariantTable();
             } catch (e) {
-                console.log(e)
-                props.refreshProductVariantTable()
-                setDisabledInputState(false)
-                swtoast.error({ text: 'Xảy ra lỗi khi mở bán vui lòng thử lại!' })
+                console.log(e);
+                props.refreshProductVariantTable();
+                setDisabledInputState(false);
+                swtoast.error({
+                    text: "Xảy ra lỗi khi mở bán vui lòng thử lại!",
+                });
             }
         } else {
             try {
-                setDisabledInputState(true)
-                await axios.put('http://localhost:8080/api/product-variant/off',
-                    { product_variant_ids: [props.product_variant_id] })
-                setDisabledInputState(false)
-                props.refreshProductVariantTable()
+                setDisabledInputState(true);
+                await axios.put(
+                    "http://localhost:8080/api/product-variant/off",
+                    { product_variant_ids: [props.product_variant_id] }
+                );
+                setDisabledInputState(false);
+                props.refreshProductVariantTable();
             } catch (e) {
-                console.log(e)
-                props.refreshProductVariantTable()
-                setDisabledInputState(false)
-                swtoast.error({ text: 'Xảy ra lỗi khi tắt sản phẩm vui lòng thử lại!' })
+                console.log(e);
+                props.refreshProductVariantTable();
+                setDisabledInputState(false);
+                swtoast.error({
+                    text: "Xảy ra lỗi khi tắt sản phẩm vui lòng thử lại!",
+                });
             }
         }
     };
@@ -107,40 +116,113 @@ const ProductAdmin = (props) => {
             .then(async (result) => {
                 if (result.isConfirmed) {
                     try {
-                        await axios.delete('http://localhost:8080/api/product-variant/delete',
-                            { data: { product_variant_ids: [props.product_variant_id] } })
-                        props.refreshProductVariantTable()
+                        await axios.delete(
+                            "http://localhost:8080/api/product-variant/delete",
+                            {
+                                data: {
+                                    product_variant_ids: [
+                                        props.product_variant_id,
+                                    ],
+                                },
+                            }
+                        );
+                        props.refreshProductVariantTable();
                         swtoast.success({
-                            text: 'Xóa biến thể sản phẩm thành công!'
-                        })
+                            text: "Xóa biến thể sản phẩm thành công!",
+                        });
                     } catch (err) {
-                        console.log(err)
+                        console.log(err);
                         swtoast.error({
-                            text: 'Xảy ra lỗi khi xóa biến thể sản phẩm vui lòng thử lại!'
-                        })
+                            text: "Xảy ra lỗi khi xóa biến thể sản phẩm vui lòng thử lại!",
+                        });
                     }
                 }
-            })
-    }
+            });
+    };
 
     return (
         <div className="table-responsive">
             <table className="table align-middle product-admin w-100">
-                <tbody className='w-100 text-center'>
+                <tbody className="w-100 text-center">
                     <tr className="w-100">
-                        <td className='col-infor-product'>
-                            <p className="name">
-                                {props.product_name + '-' + props.colour_name + '-' + props.size_name}
-                            </p>
-                            <img src={props.product_image.replace("localhost:", `localhost:${process.env.NEXT_PUBLIC_BACKEND_URL_PORT}`)} />
+                        <td
+                            className="col-infor-product"
+                            style={
+                                {
+                                    // display: "flex",
+                                }
+                            }
+                        >
+                            <div
+                                className=" d-flex justify-content-start align-items-center"
+                                style={{ width: "100%" }}
+                            >
+                                <img
+                                    src={props.product_image.replace(
+                                        "localhost:",
+                                        `localhost:${process.env.NEXT_PUBLIC_BACKEND_URL_PORT}`
+                                    )}
+                                    style={{
+                                        borderRadius: "5px",
+                                        boxShadow:
+                                            "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                                    }}
+                                />
+
+                                <p
+                                    className="name d-flex flex-column justify-content-start align-items-start"
+                                    style={{
+                                        margin: "0 20px",
+                                    }}
+                                >
+                                    <span>
+                                        Tên :{" "}
+                                        <span
+                                            style={{
+                                                fontSize: "16px",
+                                                fontWeight: "500",
+                                            }}
+                                        >
+                                            {props.product_name}
+                                        </span>
+                                    </span>
+                                    <span>
+                                        Màu :{" "}
+                                        <span
+                                            style={{
+                                                fontSize: "16px",
+                                                fontWeight: "500",
+                                            }}
+                                        >
+                                            {props.colour_name}
+                                        </span>
+                                    </span>
+                                    <span>
+                                        Size :{" "}
+                                        <span
+                                            style={{
+                                                fontSize: "16px",
+                                                fontWeight: "500",
+                                            }}
+                                        >
+                                            {props.size_name}
+                                        </span>
+                                    </span>
+                                    {/* {props.product_name +
+                                    "-" +
+                                    props.colour_name +
+                                    "-" +
+                                    props.size_name} */}
+                                </p>
+                            </div>
                         </td>
                         <td className="text-danger fw-bold col-price">
-                            <p className='d-flex align-items-center justify-content-center'>
+                            <p className="d-flex align-items-center justify-content-center">
                                 {addPointToPrice(props.price)}
                             </p>
                         </td>
                         <td className="text-danger fw-bold col-quantity">
-                            <p className='d-flex align-items-center justify-content-center'>
+                            <p className="d-flex align-items-center justify-content-center">
                                 {props.quantity}
                                 <a href="#" onClick={handleUpdateQuantity}>
                                     <span className="edit-price-button text-premium">
@@ -154,16 +236,49 @@ const ProductAdmin = (props) => {
                         </td>
                         <td className="col-action manipulation">
                             <Link href={`/product/update/${props.product_id}`}>
-                                Chỉnh sửa
+                                <button
+                                    style={{
+                                        width: "90%",
+                                        boxShadow:
+                                            " rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                                        borderRadius: "5px",
+                                        border: "none",
+                                        padding: "8px",
+                                        background: "orange",
+                                        color: "#fff",
+                                        margin: "5px 0",
+                                    }}
+                                >
+                                    Chỉnh sửa
+                                </button>
                             </Link>
                             <br />
-                            <FaTrash style={{ cursor: "pointer" }} title='Xóa' className="text-danger" onClick={() => handleDelete()} />
+                            <button
+                                style={{
+                                    width: "90%",
+                                    boxShadow:
+                                        " rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+                                    borderRadius: "5px",
+                                    border: "none",
+                                    padding: "8px",
+                                    background: "red",
+                                    color: "#fff",
+                                    margin: "5px 0",
+                                }}
+                                onClick={() => handleDelete()}
+                            >
+                                <FaTrash
+                                    style={{ cursor: "pointer" }}
+                                    title="Xóa"
+                                    className="text-white "
+                                />
+                            </button>
                         </td>
                     </tr>
                 </tbody>
             </table>
         </div>
-    )
-}
+    );
+};
 
-export default ProductAdmin
+export default ProductAdmin;
